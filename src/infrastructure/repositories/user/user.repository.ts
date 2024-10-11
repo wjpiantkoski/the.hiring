f7c8f3cac9cea5e81295ddee9bd5889e17f400dd
wjpiantkoski <wjpiantkoski@gmail.com>
2024-10-10 22:08:19 -0300

    feat: implement create method in UserRepository

---
+++ src/infrastructure/repositories/user/user.repository.ts
@@ -0,0 +1,9 @@
+import { prisma } from "@/prisma";
+import { IUserRepository } from "@/src/application/repositories/user.repository.interface";
+import { User } from "@/src/entities/models/user";
+
+export class UserRepository implements IUserRepository {
+  public async create(input: User): Promise<User> {
+    return await prisma.user.create({ data: input });
+  }
+}
