8bf6701c99304958078635819637995ff3fc4441
wjpiantkoski <wjpiantkoski@gmail.com>
2024-10-10 22:09:32 -0300

    feat: inject UserRepository in AuthenticationService

--- src/infrastructure/services/authentication/authentication.service.factory.ts
+++ src/infrastructure/services/authentication/authentication.service.factory.ts
@@ -1,7 +1,8 @@
+import { UserRepository } from "../../repositories/user/user.repository";
 import { AuthenticationService } from "./authentication.service";
 
 export class AuthenticationServiceFactory {
   static create() {
-    return new AuthenticationService();
+    return new AuthenticationService(new UserRepository());
   }
 }
--- src/infrastructure/services/authentication/authentication.service.ts
+++ src/infrastructure/services/authentication/authentication.service.ts
@@ -1,10 +1,11 @@
 import { luciaAdapter } from "@/prisma";
+import { IUserRepository } from "@/src/application/repositories/user.repository.interface";
 import { Lucia } from "lucia";
 
 export class AuthenticationService {
   private _lucia: Lucia;
 
-  constructor() {
+  constructor(private readonly userRepository: IUserRepository) {
     this._lucia = new Lucia(luciaAdapter, {
       sessionCookie: {
         expires: false,
