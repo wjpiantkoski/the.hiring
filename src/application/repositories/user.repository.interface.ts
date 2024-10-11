89cab4641295ecc1eb002a8576af25e9d790ded5
wjpiantkoski <wjpiantkoski@gmail.com>
2024-10-11 11:43:34 -0300

    feat: add method findByEmail in IUserRepository

--- src/application/repositories/user.repository.interface.ts
+++ src/application/repositories/user.repository.interface.ts
@@ -2,4 +2,5 @@ import { User } from "@/src/entities/models/user";
 
 export interface IUserRepository {
   create(input: User): Promise<User>;
+  findByEmail(email: string): Promise<User | null>;
 }
