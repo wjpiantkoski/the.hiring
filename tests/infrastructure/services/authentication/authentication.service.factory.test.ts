50c89a84a1ed77a4facef5f2f93bf6280b89d771
wjpiantkoski <wjpiantkoski@gmail.com>
2024-10-10 21:57:39 -0300

    refactor: update folders structure

--- tests/infrastructure/services/authentication/authentication.service.factory.test.ts
+++ tests/infrastructure/services/authentication/authentication.service.factory.test.ts
@@ -1,6 +1,6 @@
 import { describe, it, expect } from "vitest";
-import { AuthenticationServiceFactory } from "./authentication.service.factory";
-import { AuthenticationService } from "./authentication.service";
+import { AuthenticationServiceFactory } from "../../../../src/infrastructure/services/authentication/authentication.service.factory";
+import { AuthenticationService } from "../../../../src/infrastructure/services/authentication/authentication.service";
 
 describe("AuthenticationServiceFactory", () => {
   it("should create an instance of AuthenticationService", () => {
