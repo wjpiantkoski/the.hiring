bc80824d1df8c85fa9ad74d46e5ab8e69215364d
wjpiantkoski <wjpiantkoski@gmail.com>
2024-10-11 11:43:49 -0300

    feat: implement method findByEmail in UserRepository

--- src/infrastructure/repositories/user/user.repository.ts
+++ src/infrastructure/repositories/user/user.repository.ts
@@ -3,6 +3,10 @@ import { IUserRepository } from "@/src/application/repositories/user.repository.
 import { User } from "@/src/entities/models/user";
 
 export class UserRepository implements IUserRepository {
+  public async findByEmail(email: string): Promise<User | null> {
+    return await prisma.user.findUnique({ where: { email } });
+  }
+
   public async create(input: User): Promise<User> {
     return await prisma.user.create({ data: input });
   }
