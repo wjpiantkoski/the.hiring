acf726297327bd2854cea60e424402584d958602
wjpiantkoski <wjpiantkoski@gmail.com>
2024-10-10 22:05:07 -0300

    refactor: rename prisma client

--- prisma/index.ts
+++ prisma/index.ts
@@ -1,6 +1,6 @@
 import { PrismaAdapter } from "@lucia-auth/adapter-prisma";
 import { PrismaClient } from "@prisma/client";
 
-export const client = new PrismaClient();
+export const prisma = new PrismaClient();
 
-export const luciaAdapter = new PrismaAdapter(client.session, client.user);
+export const luciaAdapter = new PrismaAdapter(prisma.session, prisma.user);
