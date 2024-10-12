f9b07001c5f28cc081522b5550938aab0d519ee6
wjpiantkoski <wjpiantkoski@gmail.com>
2024-10-12 17:19:07 -0300

    refactor: clear main page content

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,3 @@
-import Link from "next/link";
-
 export default function Home() {
-  return (
-    <main>
-      <h1>Website</h1>
-
-      <aside>
-        <h2>Menu</h2>
-        <ul>
-          <li>
-            <Link href="/sign-in">Sign In</Link>
-          </li>
-          <li>
-            <Link href="/sign-up">Sign Up</Link>
-          </li>
-        </ul>
-      </aside>
-    </main>
-  );
+  return <main></main>;
 }
