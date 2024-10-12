894cf5dfd54a4d802b4ee30563716324982c179e
wjpiantkoski <wjpiantkoski@gmail.com>
2024-10-12 17:37:19 -0300

    fix: add type

--- app/_components/@shared/NavigationBar.tsx
+++ app/_components/@shared/NavigationBar.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
 import ThemeSwitcher from "./ThemeSwitcher";
+import React from "react";
 
 export default function NavigationBar({
   children,
   className,
-}: Readonly<{ children?: never; className?: string }>) {
+}: Readonly<{ children?: React.ReactNode; className?: string }>) {
   return (
     <nav
       className={`flex justify-between items-center h-16 px-4 py-2 border-b fixed w-full bg-stone-50 dark:bg-stone-950 ${className}`}
