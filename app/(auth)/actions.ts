adc58dea99060d6ed1873f577058c37c64b39c43
wjpiantkoski <wjpiantkoski@gmail.com>
2024-10-13 09:17:01 -0300

    feat: add signUp action

---
+++ app/(auth)/actions.ts
@@ -0,0 +1,26 @@
+"use server";
+
+import SignUpUseCaseFactory from "@/src/application/usecases/authentication/signup/signup.usecase.factory";
+import { Signup, signupSchema } from "@/src/entities/models/user";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+export async function signUp(formData: Signup) {
+  const validatedData = signupSchema.safeParse(formData);
+
+  //TODO create custom errors
+  if (!validatedData.success) {
+    throw new Error("Invalid form data");
+  }
+
+  const usecase = SignUpUseCaseFactory.create();
+  const { cookie: sessionCookie } = await usecase.execute(validatedData.data);
+
+  cookies().set(
+    sessionCookie.name,
+    sessionCookie.value,
+    sessionCookie.attributes
+  );
+
+  redirect("/dashboard");
+}
