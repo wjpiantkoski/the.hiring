f3f4c9dc5d025c503e0643ff73533fd056751459
wjpiantkoski <wjpiantkoski@gmail.com>
2024-10-13 09:08:10 -0300

    feat: add Signup usecase factory

--- src/application/usecases/authentication/signup/signup.usecase.factory.ts
+++ src/application/usecases/authentication/signup/signup.usecase.factory.ts
@@ -1,9 +1,12 @@
 import { UserRepository } from "@/src/infrastructure/repositories/user/user.repository";
 import { SignUpUsecase } from "./signup.usecase";
-import { AuthenticationService } from "@/src/infrastructure/services/authentication/authentication.service";
+import { AuthenticationServiceFactory } from "@/src/infrastructure/services/authentication/authentication.service.factory";
 
 export default class SignUpUseCaseFactory {
   static create() {
-    return new SignUpUsecase(new UserRepository(), new AuthenticationService());
+    return new SignUpUsecase(
+      new UserRepository(),
+      AuthenticationServiceFactory.create()
+    );
   }
 }
