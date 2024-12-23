799fe02c243f2db3621e12892feb0a0163b3682c
wjpiantkoski <wjpiantkoski@gmail.com>
2024-12-23 19:15:50 -0300

    feat: add ProjectFormDialog

--- src/components/projects/ProjectFormDialog.tsx
+++ src/components/projects/ProjectFormDialog.tsx
@@ -4,6 +4,7 @@ import { Project } from "@prisma/client";
 import {
   Dialog,
   DialogContent,
+  DialogFooter,
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
@@ -11,8 +12,20 @@ import { DialogTrigger } from "@radix-ui/react-dialog";
 import { Button } from "../ui/button";
 import { projectFormSchema, ProjectForm } from "@/lib/zod/schemas/project";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { FieldValues, useForm, UseFormRegister } from "react-hook-form";
+import {
+  Control,
+  FieldValues,
+  useForm,
+  UseFormRegister,
+} from "react-hook-form";
 import FormInput from "../shared/FormInput";
+import FormTextArea from "../shared/FormTextArea";
+import { Form } from "../ui/form";
+import FormDatePicker from "../shared/FormDatePicker";
+import dayjs from "dayjs";
+import { createProject } from "@/app/(protected)/projects/actions";
+import SubmitButton from "../shared/SubmitButton";
+import { useState } from "react";
 
 interface ProjectFormDialogProps {
   project?: Project;
@@ -23,8 +36,8 @@ interface ProjectFormDialogProps {
 
 const initialProjectValues = {
   name: "",
-  startDate: new Date(),
-  deadline: new Date(),
+  startDate: dayjs().toDate(),
+  deadline: dayjs().add(3, "month").toDate(),
   description: "",
 };
 
@@ -34,23 +47,30 @@ const ProjectFormDialog = ({
   isOpen,
   onClose,
 }: ProjectFormDialogProps) => {
-  const {
-    register,
-    handleSubmit,
-    reset,
-    formState: { errors },
-  } = useForm<ProjectForm>({
+  const [formErrors, setFormErrors] = useState<
+    Record<string, string[] | undefined>
+  >({});
+
+  const errorsList = Object.values(formErrors).flat();
+
+  const form = useForm<ProjectForm>({
     resolver: zodResolver(projectFormSchema),
     defaultValues: (project as ProjectForm) ?? initialProjectValues,
   });
 
-  const onSubmit = (values: ProjectForm) => {
-    console.log({ values });
-  };
-
   const handleClose = () => {
     onClose();
-    reset(initialProjectValues);
+    form.reset(initialProjectValues);
+  };
+
+  const onSubmit = async (data: ProjectForm) => {
+    const response = await createProject(data);
+
+    if (!response?.errors) {
+      handleClose();
+    }
+
+    setFormErrors(response?.errors ?? {});
   };
 
   return (
@@ -60,24 +80,74 @@ const ProjectFormDialog = ({
           <DialogTitle>{title}</DialogTitle>
         </DialogHeader>
 
-        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-2">
-          <FormInput
-            id="name"
-            label="Name"
-            errorMessage={errors.name?.message}
-            register={register as unknown as UseFormRegister<FieldValues>}
-          />
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="flex flex-col"
+          >
+            <FormInput
+              id="name"
+              label="Name"
+              errorMessage={form.formState.errors?.name?.message}
+              register={
+                form.register as unknown as UseFormRegister<FieldValues>
+              }
+            />
 
-          <div className="flex items-center space-x-2 mt-4">
-            <Button type="submit">Save</Button>
+            <FormTextArea
+              id="description"
+              label="Description"
+              errorMessage={form.formState.errors.description?.message}
+              register={
+                form.register as unknown as UseFormRegister<FieldValues>
+              }
+            />
 
-            <DialogTrigger asChild>
-              <Button type="button" variant="secondary">
-                Close
-              </Button>
-            </DialogTrigger>
-          </div>
-        </form>
+            <FormDatePicker
+              id="startDate"
+              name="startDate"
+              label="Start Date"
+              control={form.control as unknown as Control<FieldValues>}
+              register={
+                form.register as unknown as UseFormRegister<FieldValues>
+              }
+              errorMessage={form.formState.errors.startDate?.message}
+            />
+
+            <FormDatePicker
+              id="deadline"
+              name="deadline"
+              label="Deadline"
+              control={form.control as unknown as Control<FieldValues>}
+              register={
+                form.register as unknown as UseFormRegister<FieldValues>
+              }
+              errorMessage={form.formState.errors.deadline?.message}
+            />
+
+            <div className="flex items-center space-x-2 mt-4">
+              <SubmitButton pending={form.formState.isSubmitting}>
+                Save
+              </SubmitButton>
+
+              <DialogTrigger asChild>
+                <Button type="button" variant="secondary">
+                  Close
+                </Button>
+              </DialogTrigger>
+            </div>
+          </form>
+        </Form>
+
+        {!!errorsList.length && (
+          <DialogFooter>
+            {errorsList.map((error, index) => (
+              <p key={index} className="text-red-500 text-sm">
+                {error}
+              </p>
+            ))}
+          </DialogFooter>
+        )}
       </DialogContent>
     </Dialog>
   );
