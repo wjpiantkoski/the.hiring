06617c915ecf9c763dd10eac83dbf6c7b2daa6ff
wjpiantkoski <wjpiantkoski@gmail.com>
2024-12-24 11:21:44 -0300

    feat: list projects

--- src/app/(protected)/projects/page.tsx
+++ src/app/(protected)/projects/page.tsx
@@ -1,9 +1,19 @@
 import ProjectsPageHeader from "@/components/projects/ProjectsPageHeader";
+import { getProjects } from "./actions";
+import ProjectCard from "@/components/projects/ProjectCard";
 
-const Projects = () => {
+const Projects = async () => {
+  const { projects } = await getProjects();
+
   return (
-    <section>
+    <section className="flex flex-col space-y-4">
       <ProjectsPageHeader title="Projects" />
+
+      <div className="flex items-stretch flex-wrap">
+        {projects?.map((project) => (
+          <ProjectCard key={project.id} {...project} />
+        ))}
+      </div>
     </section>
   );
 };
