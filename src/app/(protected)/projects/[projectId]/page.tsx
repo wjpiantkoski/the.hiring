3665ba1cdbc0b66413315c56230c53f2f0e938ef
wjpiantkoski <wjpiantkoski@gmail.com>
2024-12-24 16:49:36 -0300

    feat: add project details page

--- src/app/(protected)/projects/[projectId]/page.tsx
+++ src/app/(protected)/projects/[projectId]/page.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import ProjectDetailsPageHeader from "@/components/projects/ProjectDetailsPageHeader";
+import { getProjectById } from "../_actions/getProjectById";
+import PageContentNotFound from "@/components/shared/PageContentNotFound";
 
-const ProjectPage = () => {
-  return <div>ProjectPage</div>;
+const ProjectPage = async ({ params }: { params: { projectId: string } }) => {
+  const { projectId } = await params;
+  const { project } = await getProjectById(projectId);
+
+  if (!project) {
+    return <PageContentNotFound content={"Project not found"} />;
+  }
+
+  return (
+    <section className="flex flex-col space-y-4">
+      <ProjectDetailsPageHeader project={project} />
+    </section>
+  );
 };
 
 export default ProjectPage;
